e140d36d4def1e520ca2c65e492ba17d5fbf68ac
avanegasp <arco527@gmail.com>
2017-12-11 11:00:14 -0500

    Only put routes and working

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,9 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+    @albums = @artist.albums.ids
+  end
 end
